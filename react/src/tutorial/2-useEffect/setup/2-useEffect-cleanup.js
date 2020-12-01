6047a925e64cd9c1bb74f9ff9263e533a6704e1b
Akash <akashpatel18041999@gmail.com>
2020-12-02 00:01:31 +0530

    commit

--- react/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,27 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size,setSize] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setSize(1000000000000000000000000)
+
+  }
+
+
+  useEffect(() => {
+    window.addEventListener('click',checkSize)
+  })
+
+  return (
+  <>
+  <h1>window</h1>
+  <h2>{size} PX</h2>
+
+
+  
+  
+  </>
+  )};
 
 export default UseEffectCleanup;
